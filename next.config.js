a79dd1c4924819ff0b61a0aa1155d6bc53b302f1
Dzevad <dzevad.del@gmail.com>
2022-04-20 00:19:57 +0200

    added intro content

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  // basePath: '/frontendmentor-intro-music',
+  // assetPrefix: '/frontendmentor-intro-music',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
